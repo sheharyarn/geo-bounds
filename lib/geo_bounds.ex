93098d496ad182d35c3e95458b1d4f4d8b03e60d
Sheharyar Naseer <hello@sheharyar.me>
2018-06-19 20:26:46 +0500

    Initial Commit

---
+++ lib/geo_bounds.ex
@@ -0,0 +1,18 @@
+defmodule GeoBounds do
+  @moduledoc """
+  Documentation for GeoBounds.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GeoBounds.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/geo_bounds_test.exs
@@ -0,0 +1,8 @@
+defmodule GeoBoundsTest do
+  use ExUnit.Case
+  doctest GeoBounds
+
+  test "greets the world" do
+    assert GeoBounds.hello() == :world
+  end
+end
